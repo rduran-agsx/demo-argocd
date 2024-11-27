426c820f49fd028ce4a8c80d933e735f8e92acc4
rsduran <reineirsamonteduran@gmail.com>
2024-11-28 06:46:31 +0800

    Updated files

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -337,7 +337,7 @@ const WelcomeComponent = ({ users, countries }) => {
             color="white"
             textAlign={{ base: "center", md: "left" }}
           >
-            Welcome to Athena5
+            Welcome to Athena6
           </Text>
           <Text
             fontSize={{ base: "16px", md: "18px" }}
