50e99be0667a1c0a98d68afaf80f2d0ccdb63640
rsduran <reineirsamonteduran@gmail.com>
2024-12-03 14:23:26 +0800

    Test frontend change

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -337,7 +337,7 @@ const WelcomeComponent = ({ users, countries }) => {
             color="white"
             textAlign={{ base: "center", md: "left" }}
           >
-            Welcome to Athena
+            Welcome to Hiraya
           </Text>
           <Text
             fontSize={{ base: "16px", md: "18px" }}
